96fd99f9fb540bcbfdf06e78f9c8ee4dced62891
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2024-08-30 21:24:14 +0200

    Update vite config to allow githubpages to render

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  }
+    plugins: [react()],
+    server: {
+        port: 3000,
+    },
+    base: '/SteamDesignClone/',
 })
